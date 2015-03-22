5a3c523805ea9b1258583cdbf319fd77da74f0f8
jakimfett <jakimfett@picraft.com>
2015-03-22 16:09:52 -0700

    Add BasicModel class

---
+++ src/main/java/jakimbox/prefab/model/BasicModel.java
@@ -0,0 +1,8 @@
+package jakimbox.prefab.model;
+
+import net.minecraft.client.model.ModelBase;
+
+public abstract class BasicModel extends ModelBase
+{
+    public abstract void render(float rotation);
+}
