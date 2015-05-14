b77d8bb6d6e6e868ee472c18bf12de6e6e78038a
jakimfett <jakimfett@picraft.com>
2015-05-13 22:59:38 -0700

    Add tab management class

---
+++ src/main/java/jakimbox/prefab/gui/Tabs.java
@@ -0,0 +1,49 @@
+package jakimbox.prefab.gui;
+
+import jakimbox.prefab.gui.tabTypes.AbstractTab;
+
+/**
+ * Class for managing multiple tabs
+ *
+ * @author jakimfett
+ */
+public class Tabs
+{
+    /**
+     * List of tabs contained in the object
+     */
+    private AbstractTab[] tabList;
+
+    /**
+     * Basic constructor
+     *
+     * @param count defines total number of possible tabs
+     */
+    public Tabs(int count)
+    {
+        tabList = new AbstractTab[count];
+    }
+
+    /**
+     * Add a tab to the list
+     *
+     * @param tab Tab object
+     * @param id  position in the tab list
+     */
+    public void addTab(AbstractTab tab, int id)
+    {
+        tabList[id] = tab;
+    }
+
+    /**
+     * Get a tab at a specific place on the list
+     *
+     * @param id tab list ID
+     * @return Tab object (or NULL)
+     */
+    public AbstractTab getTab(int id)
+    {
+        return tabList[id];
+    }
+
+}
