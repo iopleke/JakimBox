c8e9c6484743adc48cca947a30a244bfdbe6ff5d
jakimfett <jakimfett@picraft.com>
2015-05-19 22:01:47 -0700

    Add RelativeDirection reference class

---
+++ src/main/java/jakimbox/reference/RelativeDirection.java
@@ -0,0 +1,48 @@
+package jakimbox.reference;
+
+/**
+ * Rotated direction grid for use with block rotation meta values
+ *
+ * @author jakimfett
+ */
+public enum RelativeDirection
+{
+    LEFT(-1, 0), BACKLEFT(-1, 1), BACK(0, 1), BACKRIGHT(1, 1), RIGHT(1, 0), FRONTRIGHT(1, -1), FRONT(0, -1), FRONTLEFT(-1, -1);
+
+    /**
+     * Offset relative to the center block
+     */
+    public final int x, z;
+
+    RelativeDirection(int x, int z)
+    {
+        this.x = x;
+        this.z = z;
+    }
+
+    /**
+     * Returns the relative x/z for the direction
+     *
+     * @return
+     */
+    public int[] getOffset()
+    {
+        return new int[]
+        {
+            x, z
+        };
+    }
+
+    /**
+     * Returns inverse x/z direction
+     *
+     * @return
+     */
+    public int[] getOpposite()
+    {
+        return new int[]
+        {
+            -x, -z
+        };
+    }
+}
