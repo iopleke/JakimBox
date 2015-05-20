1296df929f53b2a49f7b0d7b59b1f873653292ba
jakimfett <jakimfett@picraft.com>
2015-05-19 22:31:48 -0700

    Add valid directions array

--- src/main/java/jakimbox/reference/RelativeDirection.java
+++ src/main/java/jakimbox/reference/RelativeDirection.java
@@ -10,6 +10,14 @@ public enum RelativeDirection
     LEFT(-1, 0), BACKLEFT(-1, 1), BACK(0, 1), BACKRIGHT(1, 1), RIGHT(1, 0), FRONTRIGHT(1, -1), FRONT(0, -1), FRONTLEFT(-1, -1);
 
     /**
+     * Array of all valid relative directions
+     */
+    public static final RelativeDirection[] VALID_DIRECTIONS =
+    {
+        LEFT, BACKLEFT, BACK, BACKRIGHT, RIGHT, FRONTRIGHT, FRONT, FRONTLEFT
+    };
+
+    /**
      * Offset relative to the center block
      */
     public final int x, z;
