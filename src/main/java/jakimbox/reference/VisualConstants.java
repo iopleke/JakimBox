fe994ee4b83aab5ebcbbc061b2d370abfe002092
jakimfett <jakimfett@picraft.com>
2015-05-19 22:16:08 -0700

    Add VisualConstants reference class

---
+++ src/main/java/jakimbox/reference/VisualConstants.java
@@ -0,0 +1,29 @@
+package jakimbox.reference;
+
+/**
+ * Color codes and other color related constants
+ *
+ * @author jakimfett
+ */
+public class VisualConstants
+{
+    public static final class ColorCode
+    {
+        public static final String black = "\u00A70";
+        public static final String darkBlue = "\u00A71";
+        public static final String darkCyan = "\u00A73";
+        public static final String darkGreen = "\u00A72";
+        public static final String darkGrey = "\u00A78";
+        public static final String darkRed = "\u00A74";
+        public static final String lightBlue = "\u00A79";
+        public static final String lightCyan = "\u00A7b";
+        public static final String lightGreen = "\u00A7a";
+        public static final String lightGrey = "\u00A77";
+        public static final String lightRed = "\u00A7c";
+        public static final String orange = "\u00A76";
+        public static final String pink = "\u00A7d";
+        public static final String purple = "\u00A75";
+        public static final String white = "\u00A7f";
+        public static final String yellow = "\u00A7e";
+    }
+}
