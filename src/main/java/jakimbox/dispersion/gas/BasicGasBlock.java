9850c5edcc0f74849154807b62ed814933ac7451
jakimfett <jakimfett@picraft.com>
2015-06-17 00:37:17 -0700

    Add basic type for gasses

---
+++ src/main/java/jakimbox/dispersion/gas/BasicGasBlock.java
@@ -0,0 +1,57 @@
+package jakimbox.dispersion.gas;
+
+import jakimbox.JakimBox;
+import jakimbox.prefab.block.BasicBlock;
+import jakimbox.proxy.CommonProxyBase;
+import jakimbox.reference.Naming;
+import jakimbox.registry.CreativeTabRegistry;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+/**
+ *
+ * @author jakimfett
+ */
+public class BasicGasBlock extends BasicBlock
+{
+
+    public BasicGasBlock()
+    {
+        super(JakimBox.modID, Naming.basicGas, Material.glass, Block.soundTypeSnow);
+        this.setCreativeTab(CreativeTabRegistry.TAB_JAKIMBOX);
+    }
+
+    /**
+     * Get the render type
+     *
+     * @return render ID from the CommonProxy
+     */
+    @Override
+    public int getRenderType()
+    {
+        return CommonProxyBase.RENDER_ID;
+    }
+
+    /**
+     * Disable opaque cube rendering
+     *
+     * @return false
+     */
+    @Override
+    public boolean isOpaqueCube()
+    {
+        return false;
+    }
+
+    /**
+     * Disable normal block rendering
+     *
+     * @return false
+     */
+    @Override
+    public boolean renderAsNormalBlock()
+    {
+        return false;
+    }
+
+}
---
+++ src/main/java/jakimbox/dispersion/gas/BasicGasTileEntity.java
@@ -0,0 +1,23 @@
+package jakimbox.dispersion.gas;
+
+import jakimbox.prefab.tileEntity.BasicTileEntity;
+
+/**
+ * Basic TileEntity object for creating gasses
+ *
+ * @author jakimfett
+ */
+public class BasicGasTileEntity extends BasicTileEntity
+{
+
+    /**
+     * Create a new TileEntity instance
+     *
+     * @param gasName
+     */
+    public BasicGasTileEntity(String gasName)
+    {
+        super(gasName);
+    }
+
+}
