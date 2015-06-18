52e918464bfdc9cc3d27a8e077fc798b22635b81
jakimfett <jakimfett@picraft.com>
2015-06-17 20:56:38 -0700

    Add effect infliction to gasses

--- src/main/java/jakimbox/dispersion/gas/BasicGasBlock.java
+++ src/main/java/jakimbox/dispersion/gas/BasicGasBlock.java
@@ -8,7 +8,11 @@ import jakimbox.proxy.CommonProxyBase;
 import jakimbox.reference.Naming;
 import jakimbox.registry.CreativeTabRegistry;
 import net.minecraft.block.Block;
+import net.minecraft.block.ITileEntityProvider;
 import net.minecraft.block.material.Material;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.world.World;
 
@@ -16,13 +20,14 @@ import net.minecraft.world.World;
  *
  * @author jakimfett
  */
-public class BasicGasBlock extends BasicBlock
+public class BasicGasBlock extends BasicBlock implements ITileEntityProvider
 {
 
     public BasicGasBlock()
     {
         super(JakimBox.modID, Naming.basicGas, Material.glass, Block.soundTypeSnow);
         this.setCreativeTab(CreativeTabRegistry.TAB_JAKIMBOX);
+        this.setBlockUnbreakable();
     }
 
     /**
@@ -49,6 +54,34 @@ public class BasicGasBlock extends BasicBlock
     public AxisAlignedBB getCollisionBoundingBoxFromPool(World world, int x, int y, int z)
     {
         return null;
+    }
+
+    /**
+     * Inflict effects on anything moving through the gas
+     *
+     * @param world
+     * @param x
+     * @param y
+     * @param z
+     * @param entity
+     */
+    @Override
+    public void onEntityCollidedWithBlock(World world, int x, int y, int z, Entity entity)
+    {
+        if (entity != null && entity instanceof EntityLivingBase)
+        {
+            TileEntity tileEntity = world.getTileEntity(x, y, z);
+            if (tileEntity instanceof BasicGasTileEntity)
+            {
+                ((BasicGasTileEntity) tileEntity).applyGasEffects((EntityLivingBase) entity);
+            }
+        }
+    }
+
+    @Override
+    public TileEntity createNewTileEntity(World world, int meta)
+    {
+        return new BasicGasTileEntity();
     }
 
     /**
--- src/main/java/jakimbox/dispersion/gas/BasicGasTileEntity.java
+++ src/main/java/jakimbox/dispersion/gas/BasicGasTileEntity.java
@@ -1,6 +1,12 @@
 package jakimbox.dispersion.gas;
 
 import jakimbox.prefab.tileEntity.BasicTileEntity;
+import jakimbox.reference.Naming;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.potion.PotionEffect;
 
 /**
  * Basic TileEntity object for creating gasses
@@ -9,6 +15,12 @@ import jakimbox.prefab.tileEntity.BasicTileEntity;
  */
 public class BasicGasTileEntity extends BasicTileEntity
 {
+    public List<PotionEffect> potionEffects = new ArrayList<PotionEffect>();
+
+    public BasicGasTileEntity()
+    {
+        this(Naming.basicGas);
+    }
 
     /**
      * Create a new TileEntity instance
@@ -18,6 +30,17 @@ public class BasicGasTileEntity extends BasicTileEntity
     public BasicGasTileEntity(String gasName)
     {
         super(gasName);
+
+        potionEffects.add(new PotionEffect(16, 2));
+    }
+
+    public void applyGasEffects(EntityLivingBase entity)
+    {
+        Iterator potionEffectsIterator = potionEffects.iterator();
+        while (potionEffectsIterator.hasNext())
+        {
+            entity.addPotionEffect((PotionEffect) potionEffectsIterator.next());
+        }
     }
 
 }
