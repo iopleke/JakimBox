b38baeb0d636eebdfc56093c57897bb8bb0e68fe
jakimfett <jakimfett@picraft.com>
2015-06-20 11:27:10 -0700

    Add corrosion enums

---
+++ src/main/java/jakimbox/reference/Corrodes.java
@@ -0,0 +1,11 @@
+package jakimbox.reference;
+
+/**
+ * Defines corrosion levels
+ *
+ * @author jakimfett
+ */
+public enum Corrodes
+{
+    ALL, COBBLE, DIRT, METAL, MINERAL, NONE, ORGANIC, STONE, WOOD,
+}
