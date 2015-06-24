49acd91dc93384f5d41323fdfd861bc67a4db1af
jakimfett <jakimfett@gmail.com>
2015-06-24 11:33:32 -0700

    Programmatically create eclipse scaffolding

---
+++ firstrun.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+mkdir -p eclipse/.metadata/.plugins/{org.eclipse.core.resources/.projects/Minecraft/,org.eclipse.core.runtime/.settings/}
+touch eclipse/.metadata/.plugins/org.eclipse.core.resources/.projects/Minecraft/.location
+touch eclipse/.metadata/.plugins/org.eclipse.core.runtime/.settings/org.eclipse.jdt.core.prefs
