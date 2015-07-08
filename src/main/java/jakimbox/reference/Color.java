814ae1846895d6b66db9a022ab9329c8f9c58bfb
jakimfett <jakimfett@picraft.com>
2015-07-07 22:54:04 -0700

    Add color class

---
+++ src/main/java/jakimbox/reference/Color.java
@@ -0,0 +1,51 @@
+package jakimbox.reference;
+
+/**
+ * Constants for colors
+ *
+ * @author jakimfett
+ */
+public class Color
+{
+    /**
+     * Used to add colors to chat messages
+     */
+    public static final class TextColor
+    {
+        public static final String BLACK = "\u00A70";
+        public static final String DARKBLUE = "\u00A71";
+        public static final String DARKCYAN = "\u00A73";
+        public static final String DARKGREEN = "\u00A72";
+        public static final String DARKGREY = "\u00A78";
+        public static final String DARKRED = "\u00A74";
+        public static final String LIGHTBLUE = "\u00A79";
+        public static final String LIGHTCYAN = "\u00A7b";
+        public static final String LIGHTGREEN = "\u00A7a";
+        public static final String LIGHTGREY = "\u00A77";
+        public static final String LIGHTRED = "\u00A7c";
+        public static final String ORANGE = "\u00A76";
+        public static final String PINK = "\u00A7d";
+        public static final String PURPLE = "\u00A75";
+        public static final String WHITE = "\u00A7f";
+        public static final String YELLOW = "\u00A7e";
+    }
+
+    /**
+     * True color for rendering objects
+     */
+    public static final class RenderColor
+    {
+        public static final int BLACK = -16777216;
+        public static final int BLUE = -16776961;
+        public static final int CYAN = -16711681;
+        public static final int DARKGREY = -12303292;
+        public static final int GREY = -7829368;
+        public static final int GREEN = -16711936;
+        public static final int LIGHTGREY = -3355444;
+        public static final int MAGENTA = -65281;
+        public static final int RED = -65536;
+        public static final int TRANSPARENT = 0;
+        public static final int WHITE = -1;
+        public static final int YELLOW = -256;
+    }
+}
