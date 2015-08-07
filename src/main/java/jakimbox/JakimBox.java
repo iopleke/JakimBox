8f5cef7886954f9440a3d7a672d4b8baad489afc
jakimfett <jakimfett@picraft.com>
2015-08-07 00:28:35 -0700

    Add rendering registration for JakimInTheBox

--- src/main/java/jakimbox/JakimBox.java
+++ src/main/java/jakimbox/JakimBox.java
@@ -3,8 +3,11 @@ package jakimbox;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.ModMetadata;
+import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import jakimbox.network.MessageHandler;
+import jakimbox.proxy.CommonProxyBase;
 import jakimbox.registry.BlockRegistry;
 import jakimbox.registry.CreativeTabRegistry;
 import java.util.Arrays;
@@ -27,18 +30,23 @@ public class JakimBox
     @Mod.Metadata(JakimBox.modID)
     public static ModMetadata metadata;
 
+    @SidedProxy(clientSide = "jakimbox.proxy.client.ClientProxyBase", serverSide = "jakimbox.proxy.CommonProxyBase")
+    public static CommonProxyBase proxy;
+
     @EventHandler
     public void preInit(FMLPreInitializationEvent event)
     {
         BlockRegistry.init();
         CreativeTabRegistry.init();
         Config.init();
+        MessageHandler.init();
     }
 
     @Mod.EventHandler
     public void init(FMLInitializationEvent event)
     {
         JakimBox.metadata = JakimBox.setupModMetaData(metadata);
+        proxy.registerRenderers();
     }
 
     public static ModMetadata setupModMetaData(ModMetadata meta)
---
+++ src/main/java/jakimbox/proxy/client/ClientProxyBase.java
@@ -0,0 +1,29 @@
+package jakimbox.proxy.client;
+
+import cpw.mods.fml.client.registry.ClientRegistry;
+import cpw.mods.fml.client.registry.RenderingRegistry;
+import jakimbox.box.BoxTileEntityRenderer;
+import jakimbox.box.BoxTileEntity;
+import jakimbox.prefab.render.BasicItemRenderer;
+import jakimbox.proxy.CommonProxyBase;
+import jakimbox.registry.BlockRegistry;
+import net.minecraft.item.Item;
+import net.minecraftforge.client.MinecraftForgeClient;
+
+/**
+ *
+ * @author jakimfett
+ */
+public class ClientProxyBase extends CommonProxyBase
+{
+    @Override
+    public void registerRenderers()
+    {
+        RENDER_ID = RenderingRegistry.getNextAvailableRenderId();
+
+        BoxTileEntityRenderer boxModelRenderer = new BoxTileEntityRenderer();
+        ClientRegistry.bindTileEntitySpecialRenderer(BoxTileEntity.class, boxModelRenderer);
+        MinecraftForgeClient.registerItemRenderer(Item.getItemFromBlock(BlockRegistry.boxBlock),
+            new BasicItemRenderer(boxModelRenderer, new BoxTileEntity()));
+    }
+}
