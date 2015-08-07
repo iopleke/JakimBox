f917c1260ba3d342dd006887ea18d75ec53a59c2
jakimfett <jakimfett@picraft.com>
2015-08-07 00:27:02 -0700

    Whoops.

--- src/main/java/jakimbox/reference/Naming.java
+++ src/main/java/jakimbox/reference/Naming.java
@@ -6,6 +6,6 @@ package jakimbox.reference;
  */
 public class Naming
 {
-    public static final String box = "JakimInABox";
+    public static final String box = "JakimInTheBox";
     public static final String gas = "basicGas";
 }
