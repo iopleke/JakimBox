49a4aedd96e639a8b4aa0df65e386b682a531cea
jakimfett <jakimfett@picraft.com>
2015-08-15 21:59:31 -0700

    Fix JakimBox model

--- src/main/java/jakimbox/box/BoxModel.java
+++ src/main/java/jakimbox/box/BoxModel.java
@@ -69,50 +69,50 @@ public class BoxModel extends BasicModel
         Spring4.setTextureSize(128, 128);
         Spring4.addBox(-2.5F, -1.5F, -2.5F, 5, 3, 5);
         Spring4.setRotationPoint(3.5F, 2F, -0.02000044F);
-        Head = new ModelRenderer(this, 61, 0);
+        Head = new ModelRenderer(this, 65, 64);
         Head.setTextureSize(128, 128);
-        Head.addBox(-8F, -8F, -8F, 16, 16, 16);
+        Head.addBox(-4F, -4F, -4F, 8, 8, 8);
         Head.setRotationPoint(4F, -2F, -7.152557E-07F);
-        Hat1 = new ModelRenderer(this, 39, 33);
+        Hat1 = new ModelRenderer(this, 61, 39);
         Hat1.setTextureSize(128, 128);
-        Hat1.addBox(-11F, -1F, -11F, 22, 2, 22);
+        Hat1.addBox(-5.5F, -0.5F, -5.5F, 11, 1, 11);
         Hat1.setRotationPoint(4.260691F, -2.963757F, 0.05669332F);
         Hat3 = new ModelRenderer(this, 10, 75);
         Hat3.setTextureSize(128, 128);
         Hat3.addBox(-2.5F, -1.5F, -2.5F, 5, 3, 5);
-        Hat3.setRotationPoint(5.46092F, -7.400915F, 0.3177141F);
+        Hat3.setRotationPoint(5.46092F, -7.400913F, 0.3177135F);
         Hat4 = new ModelRenderer(this, 12, 77);
         Hat4.setTextureSize(128, 128);
         Hat4.addBox(-2F, -1F, -2F, 4, 2, 4);
-        Hat4.setRotationPoint(6.035325F, -9.195881F, 0.6055787F);
+        Hat4.setRotationPoint(6.035326F, -9.195881F, 0.6055781F);
         Hat5 = new ModelRenderer(this, 14, 78);
         Hat5.setTextureSize(128, 128);
         Hat5.addBox(-1.5F, -1F, -1.5F, 3, 2, 3);
-        Hat5.setRotationPoint(6.546574F, -10.59308F, 0.9611811F);
+        Hat5.setRotationPoint(6.546574F, -10.59308F, 0.9611805F);
         Hat6 = new ModelRenderer(this, 16, 78);
         Hat6.setTextureSize(128, 128);
         Hat6.addBox(-1F, -1.5F, -1F, 2, 3, 2);
-        Hat6.setRotationPoint(7.007617F, -11.47612F, 1.468811F);
+        Hat6.setRotationPoint(7.007617F, -11.47612F, 1.46881F);
         Hat7 = new ModelRenderer(this, 18, 80);
         Hat7.setTextureSize(128, 128);
         Hat7.addBox(-0.5F, -1F, -0.5F, 1, 2, 1);
-        Hat7.setRotationPoint(7.999711F, -12.6796F, 2.906659F);
+        Hat7.setRotationPoint(7.999712F, -12.67959F, 2.906659F);
         Hat2a = new ModelRenderer(this, 69, 39);
         Hat2a.setTextureSize(128, 128);
         Hat2a.addBox(-0.5F, -0.5F, -6.5F, 1, 1, 13);
-        Hat2a.setRotationPoint(9.270021F, -0.725647F, -2.56961F);
+        Hat2a.setRotationPoint(9.270022F, -0.725647F, -2.569611F);
         Hat2c = new ModelRenderer(this, 74, 35);
         Hat2c.setTextureSize(128, 128);
         Hat2c.addBox(-5.5F, -0.5F, -0.5F, 11, 1, 1);
-        Hat2c.setRotationPoint(6.425003F, -1.026325F, 5.401099F);
+        Hat2c.setRotationPoint(6.425004F, -1.026325F, 5.4011F);
         Hat2b = new ModelRenderer(this, 69, 54);
         Hat2b.setTextureSize(128, 128);
         Hat2b.addBox(-5.5F, -0.5F, -0.5F, 11, 1, 1);
-        Hat2b.setRotationPoint(1.616548F, -2.963633F, -5.421359F);
+        Hat2b.setRotationPoint(1.616547F, -2.963633F, -5.42136F);
         Hat2 = new ModelRenderer(this, 76, 37);
         Hat2.setTextureSize(128, 128);
         Hat2.addBox(-0.5F, -0.5F, -6.5F, 1, 1, 13);
-        Hat2.setRotationPoint(-1.270021F, -3.274357F, 2.569608F);
+        Hat2.setRotationPoint(-1.270022F, -3.274357F, 2.569609F);
     }
 
     @Override
@@ -170,52 +170,52 @@ public class BoxModel extends BasicModel
         Spring4.renderWithRotation(scale);
 
         Head.rotateAngleX = -0.1621518F;
-        Head.rotateAngleY = 0.4181066F;
-        Head.rotateAngleZ = 0.2169126F;
+        Head.rotateAngleY = 0.4181067F;
+        Head.rotateAngleZ = 0.2169127F;
         Head.renderWithRotation(scale);
 
         Hat1.rotateAngleX = -0.1621518F;
         Hat1.rotateAngleY = 0.4181066F;
-        Hat1.rotateAngleZ = 0.2169126F;
+        Hat1.rotateAngleZ = 0.2169127F;
         Hat1.renderWithRotation(scale);
 
         Hat3.rotateAngleX = -0.1621518F;
-        Hat3.rotateAngleY = 0.4181066F;
+        Hat3.rotateAngleY = 0.4181067F;
         Hat3.rotateAngleZ = 0.2169126F;
         Hat3.renderWithRotation(scale);
 
-        Hat4.rotateAngleX = -0.4547836F;
-        Hat4.rotateAngleY = 0.3471911F;
-        Hat4.rotateAngleZ = 0.2386834F;
+        Hat4.rotateAngleX = -0.4547837F;
+        Hat4.rotateAngleY = 0.3471914F;
+        Hat4.rotateAngleZ = 0.2386833F;
         Hat4.renderWithRotation(scale);
 
         Hat5.rotateAngleX = -0.6104434F;
-        Hat5.rotateAngleY = 0.3010237F;
-        Hat5.rotateAngleZ = 0.2622009F;
+        Hat5.rotateAngleY = 0.301024F;
+        Hat5.rotateAngleZ = 0.2622008F;
         Hat5.renderWithRotation(scale);
 
         Hat6.rotateAngleX = -0.8446801F;
-        Hat6.rotateAngleY = 0.2063779F;
-        Hat6.rotateAngleZ = 0.3256055F;
+        Hat6.rotateAngleY = 0.2063781F;
+        Hat6.rotateAngleZ = 0.3256054F;
         Hat6.renderWithRotation(scale);
 
         Hat7.rotateAngleX = -1.007175F;
-        Hat7.rotateAngleY = 0.1025952F;
-        Hat7.rotateAngleZ = 0.4088476F;
+        Hat7.rotateAngleY = 0.1025955F;
+        Hat7.rotateAngleZ = 0.4088475F;
         Hat7.renderWithRotation(scale);
 
         Hat2a.rotateAngleX = -0.1621518F;
-        Hat2a.rotateAngleY = 0.4181067F;
+        Hat2a.rotateAngleY = 0.4181069F;
         Hat2a.rotateAngleZ = 0.2169126F;
         Hat2a.renderWithRotation(scale);
 
         Hat2c.rotateAngleX = -0.1621518F;
-        Hat2c.rotateAngleY = 0.4181067F;
+        Hat2c.rotateAngleY = 0.4181069F;
         Hat2c.rotateAngleZ = 0.2169126F;
         Hat2c.renderWithRotation(scale);
 
         Hat2b.rotateAngleX = -0.1621518F;
-        Hat2b.rotateAngleY = 0.4181067F;
+        Hat2b.rotateAngleY = 0.4181069F;
         Hat2b.rotateAngleZ = 0.2169126F;
         Hat2b.renderWithRotation(scale);
 
